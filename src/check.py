bbad6c20a74ddf51402f6c92fedff23aa23c3523
Osmo <48165006+DaFancy@users.noreply.github.com>
2021-12-28 22:42:53 +0200

    files -> "src"

---
+++ src/check.py
@@ -0,0 +1,26 @@
+import sys
+
+def isFile():
+    try:
+        file = open("path.txt", "r")
+        if file == "Siia 1. reale kleepida kausta path (Selle teksti asemele)":
+            print("Puudub muusika failide path. Loodi .txt fail nimega 'path', lisage sinna kausta path, kus on muusika failid")
+            sys.exit()
+        return file.read()
+    except:
+        print("Puudub muusika failide path. Loodi .txt fail nimega 'path', lisage sinna kausta path, kus on muusika failid")
+        file = open("path.txt", "w")
+        file.write("Siia 1. reale kleepida kausta path (Selle teksti asemele)")
+        file.close()
+        sys.exit()
+
+#Kontrollib kas faili nimi on õpitud nimede listis
+def isLearned(name, learned):
+    if name in learned:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    print("K2ivita main fail")
+    sys.exit()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,55 @@
+from pygame import *
+import vlc, choose, os, check
+from difflib import SequenceMatcher
 
+def check_similarity(a, b):
+    threshold = 0.6
+    similarity = SequenceMatcher(None, a, b).ratio()
+    print("\nSarnasus:\t", similarity)
+    if similarity > threshold: 
+        return True
+    else:
+        return False
+
+path = check.isFile()
+
+learned_songs = []
+
+while True:
+    songName = choose.randomFile(path)
+    cleanName = os.path.splitext(songName)[0]
+
+    if check.isLearned(songName, learned_songs):
+        continue
+
+    #laulu valimine ja playeri valmistamine
+    media_player = vlc.MediaPlayer()
+    media = vlc.Media(path + "/" + songName)
+    media_player.set_media(media)
+
+
+    timestamp = choose.randomTimestamp()
+
+    #laulu käima panemine
+    media_player.play()
+    #positsiooni panemine
+    media_player.set_position(timestamp)
+
+    guessName = input("\nKirjuta laulu nimi:")
+
+    print("\nSinu arvamus:\t", guessName)
+    print("Päris nimi:\t", cleanName)
+
+    while True:
+        guess = check_similarity(guessName, cleanName)
+
+        if guess == True:
+            print("\nTulemus:\tHea töö!")
+            learned_songs.append(songName)
+            break
+
+        elif guess == False:
+            print("\nTulemus:\tJäta laul meelde!")
+            break
+
+    media_player.stop()
